cf4fc09ce79763b364bd386a1984f1b635e70f9d
Robert_Raimondi <robert_raimondi@Robert-Raimondis-MacBook-Air.local>
2018-07-05 12:03:02 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-//import Greeter from './greeter';
+import Greeter from './Greeter';
 
-function Greeter() {
-  //   return 'Hello human, I am a greeter';
-  return (
-    <section>
-      <h2>'Hello human, I am a greeter'</h2>
-    </section>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <p>
+//         Lorem ipsum dolor amet tbh blue bottle kombucha vegan raw denim you
+//         probably haven't heard of them.
+//       </p>
+//       <Greeter />
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <p>
-        Lorem ipsum dolor amet tbh blue bottle kombucha vegan raw denim you
-        probably haven't heard of them.
-      </p>
-      <Greeter />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>
+          Lorem ipsum dolor amet tbh blue bottle kombucha vegan raw denim you
+          probably haven't heard of them.
+        </p>
+
+        <Greeter />
+      </div>
+    );
+  }
 }
 
 export default App;
-
-// function Person() {
-
-// }
---
+++ src/Greeter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Greeter() {
+  return (
+    <section>
+      <h2>Hello human, I am a greeter</h2>
+    </section>
+  );
+}
+
+export default Greeter;
